7bf762e261037baae74145a16a5190881fd57b07
AdamZhouLive <adamzhou@live.hk>
2014-06-20 18:10:31 +0800

    !

--- DBHelper/DBHelper/DBHelper/DataBaseAccess.cs
+++ DBHelper/DBHelper/DBHelper/DataBaseAccess.cs
@@ -7,82 +7,15 @@ using System.Data;
 
 namespace DBHelper
 {
-    public class DataBaseAccess
+    public abstract class DataBaseAccess
     {
-        #region 变量
-        private string connectionText;
-        private string server;
-        private string database;
-        private string userid;
-        private string password;
-        private string databasefile;
-        private string databasetype;
+        #region 变量      
         private IDbCommand command;
         private IDbDataAdapter dataadapter;
         private IDbConnection connection;
         #endregion
 
         #region 属性
-
-        #region string ConnectionText 数据库连接字符串
-        /// <summary>
-        /// 数据库连接字符串
-        /// </summary>
-        public string ConnectionText { get { return connectionText; } }
-        #endregion
-
-        #region string Server 数据库服务器
-        /// <summary>
-        /// 数据库服务器
-        /// </summary>
-        public string Server { get { return server; } set { server = value; UpdateDataBaseAccessInfo(); } }
-        #endregion
-
-        #region string DataBase 数据库名称
-        /// <summary>
-        /// 数据库名称
-        /// </summary>
-        public string DataBase { get { return database; } set { database = value; UpdateDataBaseAccessInfo(); } }
-        #endregion
-
-        #region string UserId 数据库用户名称
-        /// <summary>
-        /// 数据库用户名称
-        /// </summary>
-        public string UserId { get { return userid; } set { userid = value; UpdateDataBaseAccessInfo(); } }
-        #endregion
-
-        #region string Password 数据库用户密码
-        /// <summary>
-        /// 数据库用户密码
-        /// </summary>
-        public string Password { get { return password; } set { password = value; UpdateDataBaseAccessInfo(); } }
-        #endregion
-
-        #region string DataBaseFile 数据库文件
-        /// <summary>
-        /// 数据库文件
-        /// </summary>
-        public string DataBaseFile { get { return databasefile; } set { databasefile = value; UpdateDataBaseAccessInfo(); } }
-        #endregion
-
-        #region string DataBaseType 数据库类型
-        /// <summary>
-        /// 数据库类型
-        /// </summary>
-        public string DataBaseType
-        {
-            get
-            {
-                return databasetype;
-            }
-            set
-            {
-                databasetype = value.ToLower();
-                UpdateDataBaseAccessInfo();
-            }
-        }
-        #endregion
 
         #region System.Data.ConnectionState State 数据库连接状态
         /// <summary>
@@ -116,6 +49,7 @@ namespace DBHelper
             {
                 return connection;
             }
+            set { connection = value; }
         }
         #endregion 
 
@@ -129,6 +63,7 @@ namespace DBHelper
             {
                 return command;
             }
+            set { command = value; }
         }
         #endregion 
 
@@ -142,148 +77,17 @@ namespace DBHelper
             {
                 return dataadapter;
             }
+            set { dataadapter = value; }
         }
         #endregion 
 
         #endregion
 
-        #region 构造函数
+        public abstract DataTable GetDataTable(string sql);
 
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        public DataBaseAccess()
-        {
-            this.connectionText = string.Empty;
-            this.DataBaseType = string.Empty;
-            this.Server = string.Empty;
-            this.DataBase = string.Empty;
-            this.UserId = string.Empty;
-            this.Password = string.Empty;
-            this.DataBaseFile = string.Empty;
-            this.connection = null;
-        }
+        public abstract DataTable GetDataTable(string sql, Dictionary<string, string> Parameters);
 
-        /// <summary>
-        /// 构造函数
-        /// </summary>
-        /// <param name="DataBaseType">数据库类型</param>
-        /// <param name="Server">数据库服务器</param>
-        /// <param name="DataBase">数据库名字</param>
-        /// <param name="UserId">数据库用户</param>
-        /// <param name="Password">数据库用户密码</param>
-        /// <param name="DataBaseFile">数据库文件</param>
-        public DataBaseAccess(string DataBaseType, string Server, string DataBase, string UserId, string Password, string DataBaseFile)
-        {
-            this.connectionText = string.Empty;
-            this.DataBaseType = DataBaseType;
-            this.Server = Server;
-            this.DataBase = DataBase;
-            this.UserId = UserId;
-            this.Password = Password;
-            this.DataBaseFile = DataBaseFile;
-            this.connection = null;
+        public abstract IDataReader GetDataTableReader(string sql);
 
-        }
-        
-        #endregion
-
-        #region 内部方法
-
-        #region void UpdateDataBaseAccessInfo() 更新连接字符串信息
-        /// <summary>
-        /// 更新连接字符串信息
-        /// </summary>
-        private void UpdateDataBaseAccessInfo()
-        {
-            switch (databasetype)
-            {
-                case "sqlserver":
-                    connectionText = "server=" + server + @";database=" + database + @";user id=" + userid + @";pwd=" + password + ";";
-                    if (!string.IsNullOrWhiteSpace(this.ConnectionText))
-                    {
-                        this.connection = new System.Data.SqlClient.SqlConnection(this.ConnectionText);
-                        this.dataadapter = new System.Data.SqlClient.SqlDataAdapter();
-                    }
-                    break;
-                case "access":
-                    connectionText = "provider=microsoft.jet.oledb.4.0;data source='" + databasefile + "'";
-                    if (!string.IsNullOrWhiteSpace(this.ConnectionText))
-                    {
-                        this.connection = new System.Data.OleDb.OleDbConnection(this.ConnectionText);
-                        this.dataadapter = new System.Data.OleDb.OleDbDataAdapter();
-                    }
-                    break;
-                case "mysql":
-                    connectionText = "server=" + server + @";database=" + database + @";uid=" + userid + @";pwd=" + password + ";";
-                    if (!string.IsNullOrWhiteSpace(this.ConnectionText))
-                    {
-                        this.connection = new MySql.Data.MySqlClient.MySqlConnection(this.ConnectionText);
-                        this.dataadapter = new MySql.Data.MySqlClient.MySqlDataAdapter();
-                    }
-                    break;
-                case "excel":
-                    connectionText = "Provider=Microsoft.Jet.OLEDB.12.0;Driver={Microsoft Excel Driver (*.xls, *.xlsx, *.xlsm, *.xlsb)};DBQ="+databasefile+";";
-                    if (!string.IsNullOrWhiteSpace(this.ConnectionText))
-                    {
-                        this.connection = new System.Data.OleDb.OleDbConnection(this.ConnectionText);
-                        this.dataadapter = new System.Data.OleDb.OleDbDataAdapter();
-                      
-                    }
-                    break;
-                case "oracle":
-                    connectionText = "Driver={Microsoft ODBC for Oracle};Server="+server+";Uid="+userid+";Pwd="+password+";";
-                    if (!string.IsNullOrWhiteSpace(this.ConnectionText))
-                    {
-                        this.connection = new System.Data.Odbc.OdbcConnection(this.ConnectionText);
-                        this.dataadapter = new System.Data.Odbc.OdbcDataAdapter();
-                    }
-                    break;
-            }
-        }
-        #endregion 
-
-        #endregion
-
-        public DataTable GetDataTable(string sql)
-        {
-            DataTable dt = new DataTable();
-            this.connection.Open();
-            this.command = this.connection.CreateCommand();
-            this.command.CommandType = CommandType.Text;
-            this.command.CommandText = sql;
-
-            this.dataadapter.SelectCommand = this.command;
-            DataSet ds=new DataSet();
-            this.dataadapter.Fill(ds);
-            dt = ds.Tables[0];
-
-            ds.Dispose();
-            this.connection.Close();
-            return dt;
-        }
-
-        public DataTable GetDataTable(string sql,System.Data.IDataParameterCollection parameters)
-        {
-            DataTable dt = new DataTable();
-            this.connection.Open();
-            this.command = this.connection.CreateCommand();
-            this.command.CommandType = CommandType.Text;
-            this.command.CommandText = sql;
-            this.command.CreateParameter();
-            foreach (IDataParameter parameter in parameters) 
-            {
-                this.command.Parameters.Add(parameter);
-            }
-
-            this.dataadapter.SelectCommand = this.command;
-            DataSet ds = new DataSet();
-            this.dataadapter.Fill(ds);
-            dt = ds.Tables[0];
-
-            ds.Dispose();
-            this.connection.Close();
-            return dt;
-        }
     }
 }
--- DBHelper/DBHelper/DBHelper/SqlServer.cs
+++ DBHelper/DBHelper/DBHelper/SqlServer.cs
@@ -8,33 +8,90 @@ using System.Data;
 
 namespace DBHelper
 {
-    public class SqlServer
+    public class SqlServer:DataBaseAccess
     {
-
         public SqlServer()
         {
-            this.Server = string.Empty;
-            this.DataBase = string.Empty;
-            this.UserId = string.Empty;
-            this.Password = string.Empty;
+            this.Connection = null;
+            this.Command = null;
+            this.DataAdapter = null;
         }
 
         public SqlServer(string ConnectionText)
         {
-            this.Server = string.Empty;
-            this.DataBase = string.Empty;
-            this.UserId = string.Empty;
-            this.Password = string.Empty;
-            this.connectionText = ConnectionText;
+            this.Connection = new System.Data.SqlClient.SqlConnection(ConnectionText);
+            this.Command = this.Connection.CreateCommand();
+            this.DataAdapter = new System.Data.SqlClient.SqlDataAdapter();
         }
-
-        public SqlServer(string Server, string DataBase, string UserId, string Password)
+        public SqlServer(string Server, string DataBaseName, string UserId, string Password)
         {
-            this.Server = Server;
-            this.DataBase = DataBase;
-            this.UserId = UserId;
-            this.Password = Password;
+            string ConnectionText = "server=" + Server + @";database=" + DataBaseName + @";user id=" + UserId + @";pwd=" + Password + ";";
+            this.Connection = new System.Data.SqlClient.SqlConnection(ConnectionText);
+            this.Command = this.Connection.CreateCommand();
+            this.DataAdapter = new System.Data.SqlClient.SqlDataAdapter();
+            
         }
 
+        public override DataTable GetDataTable(string sql)
+        {
+            DataTable dt = new DataTable();
+            this.Connection.Open();
+            this.Command = this.Connection.CreateCommand();
+            this.Command.CommandType = CommandType.Text;
+            this.Command.CommandText = sql;
+
+            this.DataAdapter.SelectCommand = this.Command;
+            DataSet ds = new DataSet();
+            this.DataAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            ds.Dispose();
+            this.Connection.Close();
+            return dt;
+        }
+
+        public override DataTable GetDataTable(string sql, Dictionary<string, string> Parameters)
+        {
+
+            DataTable dt = new DataTable();
+            this.Connection.Open();
+            this.Command = this.Connection.CreateCommand();
+            this.Command.CommandType = CommandType.Text;
+            this.Command.CommandText = sql;
+            this.Command.CreateParameter();
+            foreach (string key in Parameters.Keys)
+            {
+                this.Command.Parameters.Add(new System.Data.SqlClient.SqlParameter(key,Parameters[key]));
+            }
+
+            this.DataAdapter.SelectCommand = this.Command;
+            DataSet ds = new DataSet();
+            this.DataAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            ds.Dispose();
+            this.Connection.Close();
+            return dt;
+        }
+
+
+        public override IDataReader GetDataTableReader(string sql)
+        {
+
+            DataTable dt = new DataTable();
+            this.Connection.Open();
+            this.Command = this.Connection.CreateCommand();
+            this.Command.CommandType = CommandType.Text;
+            this.Command.CommandText = sql;
+
+            this.DataAdapter.SelectCommand = this.Command;
+            DataSet ds = new DataSet();
+            this.DataAdapter.Fill(ds);
+            dt = ds.Tables[0];
+
+            ds.Dispose();
+            this.Connection.Close();
+            return dt.CreateDataReader();
+        }
     }
 }
